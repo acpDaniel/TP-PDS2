a0910c1bda44d2208f6c37e9b87536d946afb5df
Leonardo de Sá <leonardodesarocha@gmail.com>
2022-10-21 13:08:06 -0300

    arquivo main.cpp criado

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <vector>
+#include <list>
+
+#include "baralho.hpp"
+#include "carta.hpp"
+
+int main() {
+
+    return 0;
+}
