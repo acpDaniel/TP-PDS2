14806c40a316cd3f801e6dca1ea4bc8cfb4143eb
Leonardo de Sá <leonardodesarocha@gmail.com>
2022-11-20 21:21:31 -0300

    Biblioteca tpstring criada

---
+++ include/tpstring.hpp
@@ -0,0 +1,20 @@
+#ifndef TPSTRING_HPP
+#define TPSTRING_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+#include <stdlib.h>
+#include <set>
+#include <vector>
+
+#define MAXTAM_NAME 20
+#define MINTAM_NAME 3
+
+#define MAXTAM_PASSWORD 4
+#define MINTAM_PASSWORD 15
+
+#endif
