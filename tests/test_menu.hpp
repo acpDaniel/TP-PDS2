15de197e8e6b4cdffc845f795dfb40a9e0de3e07
Daniel <danielprojetos777@gmail.com>
2022-11-26 17:55:54 -0300

    testes da classe menu por enqt comentados

---
+++ tests/test_menu.hpp
@@ -0,0 +1,29 @@
+//#include "catch.hpp"
+#include "doctest.hpp"
+
+#include "../include/carta.hpp"
+#include "../include/baralho.hpp"
+#include "../include/jogador.hpp"
+#include "../include/bot.hpp"
+#include "../include/humano.hpp"
+#include "../include/rodada.hpp"
+#include "../include/menu.hpp"
+#include "../include/jogo.hpp"
+
+#include <cstdint>
+#include <cmath>
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <list>
+
+TEST_CASE("TESTANDO RETORNO NOME USUARIO")
+{
+    // fazer a funçao cadastro retornar o nome tb
+    // Menu m1 = Menu();
+    // m1.cadastroUsuario();
+    // CHECK_EQ("dani", m1.loginUsuario());
+}
--- tests/truco_test.cpp
+++ tests/truco_test.cpp
@@ -9,16 +9,4 @@
 #include "test_bot.hpp"
 #include "test_humano.hpp"
 #include "test_rodada.hpp"
-
-TEST_CASE("TESTANDO ID JOGADOR MAIOR CARTA")
-{
-    // esse metodo sempre eh testado passando como parametro o pesos das cartas dos jogadores com id 0, 1 , 2 e 3 respectivamente
-    Rodada b1 = Rodada();
-    CHECK_EQ(0, b1.idJogadorMaiorCarta(14, 2, 1, 0));
-    CHECK_EQ(1, b1.idJogadorMaiorCarta(5, 12, 1, 0));
-    CHECK_EQ(2, b1.idJogadorMaiorCarta(3, 6, 9, 0));
-    CHECK_EQ(3, b1.idJogadorMaiorCarta(4, 3, 7, 11));
-    // se tiver dois com mesmo valor ele considera o primeiro que tacou a carta
-    CHECK_EQ(2, b1.idJogadorMaiorCarta(4, 3, 11, 11));
-    CHECK_EQ(0, b1.idJogadorMaiorCarta(14, 14, 2, 3));
-}
+#include "test_menu.hpp"
