dd2ae8ab3948b3ada63ca950a47fef2cb2a622ad
Daniel <danielprojetos777@gmail.com>
2022-12-04 19:27:19 -0300

    finalizaçao testes classe jogo

--- tests/test_jogo.hpp
+++ tests/test_jogo.hpp
@@ -125,7 +125,112 @@ TEST_CASE("TESTANDO PEDIDOS TRUCO JOGADOR")
         Jogo j1 = Jogo();
         // caso usuario digite uma entrada invalida, numero diferente de 1 e 2
         CHECK_THROWS(j1.checaPedidoTrucoJogador());
-        // caso usuario digite uma entrada invalida, letra
+        // caso usuario digite uma entrada invalida, numero diferente de 1 e 2
         CHECK_THROWS(j1.checaPedidoTrucoJogador());
     }
+}
+
+TEST_CASE("TESTANDO SEQUENCIAS JOGADAS INCIANDO POR USUARIO")
+{
+    // nao pede truco, joga 1 carta
+    srand(1);
+    Jogo j1 = Jogo();
+    j1.inicializaBaralho();
+    j1.usuario.setMao(j1.baralhoDaQueda.getMaoJogador0());
+    j1.bot1.setMao(j1.baralhoDaQueda.getMaoJogador1());
+    j1.bot2.setMao(j1.baralhoDaQueda.getMaoJogador2());
+    j1.bot3.setMao(j1.baralhoDaQueda.getMaoJogador3());
+    // com as jogadas programadas no arquivo eh para dupla 2 ganhar a queda
+    // dupla 1: cartas peso 7 e 8 , dupla 2: cartas peso 3 e 9
+    j1.sequenciaJogadasIniciandoUsuario();
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla1());
+    CHECK_EQ(1, j1.rodadaAtual.getVitoriasParciaisDupla2());
+
+    // como o bot3 tacou a maior carta, o maiorId tem que ser 3
+    CHECK_EQ(3, j1.rodadaAtual.getMaiorIdMd3());
+}
+
+TEST_CASE("TESTANDO SEQUENCIAS JOGADAS INCIANDO POR BOT1")
+{
+    // nao pede truco, joga 1 carta
+    srand(200);
+    Jogo j1 = Jogo();
+    j1.inicializaBaralho();
+    j1.usuario.setMao(j1.baralhoDaQueda.getMaoJogador0());
+    j1.bot1.setMao(j1.baralhoDaQueda.getMaoJogador1());
+    j1.bot2.setMao(j1.baralhoDaQueda.getMaoJogador2());
+    j1.bot3.setMao(j1.baralhoDaQueda.getMaoJogador3());
+    // com as jogadas programadas no arquivo eh para dupla 1 ganhar a queda
+    // dupla 1: cartas peso 2 e 10 , dupla 2: cartas peso 7 e 6
+    j1.sequenciaJogadasIniciandoBot1();
+    CHECK_EQ(1, j1.rodadaAtual.getVitoriasParciaisDupla1());
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla2());
+
+    // como o bot2 tacou a maior carta, o maiorId tem que ser 2
+    CHECK_EQ(2, j1.rodadaAtual.getMaiorIdMd3());
+}
+
+TEST_CASE("TESTANDO SEQUENCIAS JOGADAS INCIANDO POR BOT2")
+{
+    // nao aceita truco do bot, nao pede truco , joga carta 1
+    srand(11);
+    Jogo j1 = Jogo();
+    j1.inicializaBaralho();
+    j1.usuario.setMao(j1.baralhoDaQueda.getMaoJogador0());
+    j1.bot1.setMao(j1.baralhoDaQueda.getMaoJogador1());
+    j1.bot2.setMao(j1.baralhoDaQueda.getMaoJogador2());
+    j1.bot3.setMao(j1.baralhoDaQueda.getMaoJogador3());
+    // com as jogadas programadas no arquivo eh para dupla 1 ganhar a queda
+    // dupla 1: cartas peso 5 e 7 , dupla 2: cartas peso 3 e 3
+    j1.sequenciaJogadasIniciandoBot2();
+    CHECK_EQ(1, j1.rodadaAtual.getVitoriasParciaisDupla1());
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla2());
+
+    // como o usuario tacou a maior carta, o maiorId tem que ser 0
+    CHECK_EQ(0, j1.rodadaAtual.getMaiorIdMd3());
+}
+
+TEST_CASE("TESTANDO SEQUENCIAS JOGADAS INCIANDO POR BOT3")
+{
+    // nao pede truco , joga carta 1
+    srand(56);
+    Jogo j1 = Jogo();
+    j1.inicializaBaralho();
+    j1.usuario.setMao(j1.baralhoDaQueda.getMaoJogador0());
+    j1.bot1.setMao(j1.baralhoDaQueda.getMaoJogador1());
+    j1.bot2.setMao(j1.baralhoDaQueda.getMaoJogador2());
+    j1.bot3.setMao(j1.baralhoDaQueda.getMaoJogador3());
+    // com as jogadas programadas no arquivo eh para dupla 1 ganhar a queda
+    // dupla 1: cartas peso 3 e 14 , dupla 2: cartas peso 8 e 7
+    j1.sequenciaJogadasIniciandoBot3();
+    CHECK_EQ(1, j1.rodadaAtual.getVitoriasParciaisDupla1());
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla2());
+
+    // como o usuario tacou a maior carta, o maiorId tem que ser 0
+    CHECK_EQ(0, j1.rodadaAtual.getMaiorIdMd3());
+}
+
+TEST_CASE("TESTANDO PROCEDIMENTOS FINAL DA RODADA")
+{
+    // acontece uma sequencia de jogadas
+    srand(56);
+    Jogo j1 = Jogo();
+    j1.inicializaBaralho();
+    j1.usuario.setMao(j1.baralhoDaQueda.getMaoJogador0());
+    j1.bot1.setMao(j1.baralhoDaQueda.getMaoJogador1());
+    j1.bot2.setMao(j1.baralhoDaQueda.getMaoJogador2());
+    j1.bot3.setMao(j1.baralhoDaQueda.getMaoJogador3());
+    j1.sequenciaJogadasIniciandoBot3();
+
+    // depois podemos chamar a procedimentos final, e checar se tudo foi resetado
+    j1.procedimentosFinalRodada();
+    // reseta a pontuacao
+    CHECK_EQ(2, j1.rodadaAtual.getPontuacaoRodadaAtual());
+    // reseta as vitorias parciais
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla1());
+    CHECK_EQ(0, j1.rodadaAtual.getVitoriasParciaisDupla2());
+    // reseta quantidade quedas rodada atual
+    CHECK_EQ(0, j1.rodadaAtual.getQtdQuedasRodadaAtual());
+    // reseta o baralho (40 cartas) e ja destribui 3 para cada jogador (12 no total)
+    CHECK_EQ(28, j1.baralhoDaQueda.getBaralhoSize());
 }
