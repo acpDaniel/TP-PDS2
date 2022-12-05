da49f857a0e5785c46369b97035e1ac5fbda1499
Daniel <danielprojetos777@gmail.com>
2022-12-04 21:07:13 -0300

    testes classe menu

--- tests/test_menu.hpp
+++ tests/test_menu.hpp
@@ -20,10 +20,98 @@
 #include <vector>
 #include <list>
 
-TEST_CASE("TESTANDO RETORNO NOME USUARIO")
+TEST_CASE("TESTANDO VERIFCACAO TAM MAX STRING")
 {
-    // fazer a funçao cadastro retornar o nome tb
-    // Menu m1 = Menu();
-    // m1.cadastroUsuario();
-    // CHECK_EQ("dani", m1.loginUsuario());
+    // retorna 1 caso ultrapasse o tam max
+    SUBCASE("CASO POSITIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(1, m1.strMaxTamChecker("teste", 2));
+        CHECK_EQ(1, m1.strMaxTamChecker("dan", 1));
+        CHECK_EQ(1, m1.strMaxTamChecker("paralelepipedo", 7));
+    }
+    // deveria retornar 0 pois nao vai ultrapassar
+    SUBCASE("CASO NEGATIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(1, m1.strMaxTamChecker("teste", 5));
+        CHECK_EQ(1, m1.strMaxTamChecker("dan", 4));
+        CHECK_EQ(1, m1.strMaxTamChecker("paralelepipedo", 33));
+    }
+}
+
+TEST_CASE("TESTANDO VERIFCACAO TAM MIN STRING")
+{
+    // retorna 1 caso nao ultrapasse o tamanho minimo
+    SUBCASE("CASO POSITIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(0, m1.strMinTamChecker("teste", 2));
+        CHECK_EQ(0, m1.strMinTamChecker("dan", 1));
+        CHECK_EQ(0, m1.strMinTamChecker("paralelepipedo", 7));
+    }
+    // deveria retornar 0 pois vai ultrapassar tamanho minimo
+    SUBCASE("CASO NEGATIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(1, m1.strMinTamChecker("teste", 4));
+        CHECK_EQ(1, m1.strMinTamChecker("dan", 2));
+        CHECK_EQ(1, m1.strMinTamChecker("paralelepipedo", 5));
+    }
+}
+
+TEST_CASE("TESTANDO VERIFCACAO CONTEM ESPAÇO STRING")
+{
+    // retorna 1 caso tenha espaco na string
+    SUBCASE("CASO POSITIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(0, m1.strSpaceChecker("teste"));
+        CHECK_EQ(0, m1.strSpaceChecker("dan"));
+        CHECK_EQ(0, m1.strSpaceChecker("paralelepipedo"));
+    }
+    // deveria retornar 1 pois tem espaco
+    SUBCASE("CASO NEGATIVO")
+    {
+        Menu m1 = Menu();
+        CHECK_EQ(0, m1.strSpaceChecker("tes te"));
+        CHECK_EQ(0, m1.strSpaceChecker("d an"));
+        CHECK_EQ(0, m1.strSpaceChecker("para lelepipedo"));
+    }
+}
+
+TEST_CASE("TESTANDO SE ACHA O USARNAME NO ARQUIVO TXT")
+{
+    // vai retornar 1 se tiver o nome procurado no arquivo que salva o historico
+    Menu m1 = Menu();
+    CHECK_EQ(1, m1.findName("dani"));
+    CHECK_EQ(1, m1.findName("leo"));
+    // retornar 0 pois nao vai achar o usuario
+    CHECK_EQ(0, m1.findName("nomeinvalido"));
+}
+
+TEST_CASE("TESTANDO SE SENHA CONDIZ COM A CADASTRADA")
+{
+    Menu m1 = Menu();
+    CHECK_EQ(1, m1.passwordChecker("dani", "dani"));
+    CHECK_EQ(1, m1.passwordChecker("leo", "leoo"));
+    // retornar 0 pois nao vai achar o usuario
+    CHECK_EQ(0, m1.passwordChecker("nomeinvalido", "senhainvalida"));
+}
+
+TEST_CASE("TESTANDO CADASTRO DO USUARIO")
+{
+    // cadastra e procura pelo cadastro no arquivo que salva informacoes
+    Menu m1 = Menu();
+    m1.playerCreate("usuarioteste", "senhateste");
+    CHECK_EQ(1, m1.findName("usuarioteste"));
+    CHECK_EQ(1, m1.passwordChecker("usuarioteste", "senhateste"));
+}
+
+TEST_CASE("TESTANDO LOGIN USUARIO")
+{
+    // funcao que retorna o nome do jogador que esta logado, tem que digitar usuario e senha
+    // tem que digitar usuario que existe e dps senha que bate com esse usuario
+    Menu m1 = Menu();
+    CHECK_EQ("dani", m1.loginUsuario());
 }
